e64ca4959a00a2885045f0708c9b983641571e9b
Sheikh Muhammad Ahmed <ahmed.arif3960@gmail.com>
2025-05-23 16:25:49 +0500

    Complete BankingManagementSystem class

--- Basics/Main.java
+++ Basics/Main.java
@@ -1,5 +1,10 @@
 package Basics;
-
 public class Main {
-    
+    public static void main(String[] args) {
+BankingManagementSystem Accouunt1 = new BankingManagementSystem("Ahmed Arif");
+Accouunt1.Details();
+Accouunt1.deposit(2000);
+Accouunt1.withdrawl(1500);
+Accouunt1.checkBalance();
+    }
 }
